118baed42a7853f15c62ad015f9117ebb5bccae1
sofiree <muosophia0@gmail.com>
2021-12-15 08:56:45 +0100

    Fixex to toutes and auth css styling

--- Authentication/script_123405.js
+++ Authentication/script_123405.js
@@ -1,45 +1,45 @@
-let myForm = document.forms['Signup']
-const name_val = myForm['Name']
-const email = myForm['Email']
-const username = myForm['Username']
-const password = myForm["Password"]
-const confirmPassword = myForm['confirmPassword']
-const gender = myForm['gender']
+// let myForm = document.forms['Signup']
+// const name_val = myForm['Name']
+// const email = myForm['Email']
+// const username = myForm['Username']
+// const password = myForm["Password"]
+// const confirmPassword = myForm['confirmPassword']
+// const gender = myForm['gender']
 
-myForm.addEventListener("click", (event)=>{
-    event.preventDefault();
+// myForm.addEventListener("click", (event)=>{
+//     event.preventDefault();
 
 
 
-    if( name_val.value ==="" ||
-        username.value === "" ||
-        password.value === "" 
-    ){
-        const myInput =[name_val, email, username,password,confirmPassword,gender]
+//     if( name_val.value ==="" ||
+//         username.value === "" ||
+//         password.value === "" 
+//     ){
+//         const myInput =[name_val, email, username,password,confirmPassword,gender]
 
-        myInput.forEach((el)=>{
-            if(el.value === ""){
-                el.style = "border:2px solid red"
-            }
-        })
-    }else(password.value !== confirmPassword.value);
-        {el.style = "border:2px solid red"
-    };
+//         myInput.forEach((el)=>{
+//             if(el.value === ""){
+//                 el.style = "border:2px solid red"
+//             }
+//         })
+//     }else(password.value !== confirmPassword.value);
+//         {el.style = "border:2px solid red"
+//     };
 
-    if(!emailValidation(email.value)){
-        email.style = "border:2px solid red"
-    }
+//     if(!emailValidation(email.value)){
+//         email.style = "border:2px solid red"
+//     }
 
     
-    if(!nameValidation(name_val.value)){
-        name_val.style = "border:2px solid red"
-    }
+//     if(!nameValidation(name_val.value)){
+//         name_val.style = "border:2px solid red"
+//     }
 
-function nameValidation(name_val){
-    const names = name_val.split(" ")
-    return names.length > 1
-}
+// function nameValidation(name_val){
+//     const names = name_val.split(" ")
+//     return names.length > 1
+// }
 
-function emailValidation(mail){
-    const email = mail.split("@")
-    return email.length === 1}})
+// function emailValidation(mail){
+//     const email = mail.split("@")
+//     return email.length === 1}})

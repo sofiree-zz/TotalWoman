6ec5cc4b69d50e9b94b3b484fcfbd0a7fc2675ab
sofiree <muosophia0@gmail.com>
2021-12-17 09:52:23 +0100

    served static assets

--- webcage/index.js
+++ webcage/index.js
@@ -1,6 +1,12 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+const formatMessage = require('./helpers/formatDate')
+
 const app = express()
 
 const PORT =3000 || process.env.PORT
 
+app.use(express.static(path.join(__dirname, 'ChatApp')))
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`))

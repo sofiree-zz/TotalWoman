2059cafe8b0dc29bf5a395eb7aabad9a3fb6c6e0
sofiree <muosophia0@gmail.com>
2021-12-17 10:22:09 +0100

    fixes on JSON file(nodemon)

--- webcage/index.js
+++ webcage/index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-const formatMessage = require('./helpers/formatDate')
+// const formatMessage = require('./helpers/formatDate')
+// const {
+//     getActiveuser,
+//     exitForum,
+//     getIndividualUser,
+//     newUser
+// } = require('./helpers/userHelper');
+
 
 const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT =3000 || process.env.PORT
 
-app.use(express.static(path.join(__dirname, 'ChatApp')))
+// i could also set the public folder this like this (__dirname, './../Public') incase the public folder is not in the same directory
+app.use(express.static(path.join(__dirname, 'Public')));
+
+
+
+
+
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`))

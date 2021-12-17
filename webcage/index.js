441c23c00e91df9b92b5abe4b4cfe0d91f3e3712
sofiree <muosophia0@gmail.com>
2021-12-17 10:42:40 +0100

    created Express server & initialize sicket,io server

--- webcage/index.js
+++ webcage/index.js
@@ -2,25 +2,46 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-// const formatMessage = require('./helpers/formatDate')
-// const {
-//     getActiveuser,
-//     exitForum,
-//     getIndividualUser,
-//     newUser
-// } = require('./helpers/userHelper');
+const formatMessage = require('./helpers/formatDate')
+const {
+    getActiveuser,
+    exitForum,
+    getIndividualUser,
+    newUser
+} = require('./helpers/userHelper');
 
 
 const app = express()
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT =3000 || process.env.PORT
+const PORT = 3000 || process.env.PORT
 
 // i could also set the public folder this like this (__dirname, './../Public') incase the public folder is not in the same directory
 app.use(express.static(path.join(__dirname, 'Public')));
 
+io.on('connection', socket => {
+    socket.on('joinForum', ({ email, password }) => {
+        const user = newUser(socket.id, email, forum);
 
+        socket.join(user.forum);
+
+        // broadcast everytime someone joins the forum
+        socket.broadcast
+            .to(user.forum)
+            .emit('message', formatMessage("TotalWoman", `${user.email} has joined the chat`));
+
+
+            // current user and forum
+            io.to(user.forum).emit('forumUsers', {
+                forum: user.forum,
+                users: getIndividualUsers(user.forum)
+            });
+    });
+
+
+    
+})
 
 
 
